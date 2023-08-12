845c75c0f8943299af97f5f9334a982c46949fde
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2023-08-12 12:04:59 +0200

    Annotate plot, Fix api limit error

--- rpc_client.py
+++ rpc_client.py
@@ -40,7 +40,7 @@ with col3:
 
 
 
-def plot_embeddings_2d(embeddings_2d, labels=None):
+def plot_embeddings_2d(embeddings_2d, keys=None, labels=None):
     fig, ax = plt.subplots()
 
     # If labels are provided, use them for coloring
@@ -51,6 +51,10 @@ def plot_embeddings_2d(embeddings_2d, labels=None):
             ax.scatter(embeddings_2d[idxs, 0], embeddings_2d[idxs, 1], label=label)
     else:
         ax.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+        # Annotate each point with its key if keys are provided
+        if keys is not None:
+            for idx, key in enumerate(keys):
+                ax.annotate(key, (embeddings_2d[idx, 0], embeddings_2d[idx, 1]))
 
     ax.set_title("2D visualization of embeddings")
     if labels is not None:
@@ -121,16 +125,20 @@ def get_abi_from_contr_addr(contract_address):
     response_json = response.json()
     abi_json = response_json['result']
     contract_abi = web3.eth.contract(abi=abi_json).abi
-    with col2:
-        with st.expander("Contract ABI"):
-            st.write(contract_abi)
-        return contract_abi
+    # with col2:
+    #     with st.expander("Contract ABI"):
+    #         st.write(contract_abi)
+    return contract_abi
 
 
 
 if contract_address:
-    abi_json = get_abi_from_contr_addr(contract_address)
+    # UNCOMMENTED DUE TO API LIMIT REACHED
+    # abi_json = get_abi_from_contr_addr(contract_address)
     abi_json = '[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"balance","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"operator","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"owner","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+    with col2:
+        with st.expander("Contract ABI"):
+            st.write(abi_json)
     #from arbiscan
     # abi_json = '[{"inputs":[{"internalType":"address","name":"_logic","type":"address"},{"internalType":"address","name":"admin_","type":"address"},{"internalType":"bytes","name":"_data","type":"bytes"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":false,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"beacon","type":"address"}],"name":"BeaconUpgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"admin_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"implementation_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"stateMutability":"payable","type":"receive"}]'
     nft_contract = web3.eth.contract(address=contract_address, abi=abi_json)
@@ -236,7 +244,8 @@ embeddings_2d = reduce_dimensions(embeddings, method='PCA')
 
 # Plot in Streamlit
 st.write("Plotting PCA for DESCRIPTION EMBEDDINGS")
-plot_embeddings_2d(embeddings_2d)
+emb_keys_list = list(st.session_state['embeddings'].keys())
+plot_embeddings_2d(embeddings_2d, keys=emb_keys_list)
 def cluster_embeddings(embeddings, n_clusters=3):
     kmeans = KMeans(n_clusters=n_clusters)
     return kmeans.fit_predict(embeddings)
